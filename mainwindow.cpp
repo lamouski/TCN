d597f649df88ab70e496347831d0e1574367ca95
lamouski <lamovsky@gmail.com>
2018-05-09 21:53:51 +0200

    license is added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,21 @@
+/*
+ * TCN Field Booking Application
+ * Copyright (C) 2018 Dzianis Lamouski
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -44,7 +62,7 @@ void MainWindow::on_delete_pushButton_clicked()
     DbManager* db = DbManager::instance();
 
     db->addMember("Denis", "Lamouski");
-    db->addMember("Natallia", "Lamouskaya");
+    db->addMember("Natasha", "Lind");
     db->addMember("Peter", "Mayer");
     db->addMember("Viktor", "Stadtler");
     db->addMember("Hans", "Koch");
